08318dc27fdc71674f5ae16f87efa512aecd4041
kiyosuke <k.kiyosuke1999@gmail.com>
2019-07-20 08:41:11 +0900

    BottomNavigationViewを使ったサンプルを追加

--- app/src/main/java/com/kiyosuke/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/kiyosuke/bottomnavigation/MainActivity.kt
@@ -2,11 +2,31 @@ package com.kiyosuke.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation
+            .addTab(R.drawable.ic_home, "ホーム")
+            .addTab(R.drawable.ic_message, "メッセージ")
+            .addTab(R.drawable.ic_people, "フレンド")
+            .addTab(R.drawable.ic_shopping_cart, "買い物")
+            .addTab(R.drawable.ic_train, "時刻表")
+            .addTab(R.drawable.ic_map, "地図")
+
+        bottomNavigation.setOnTabSelectedListener { position ->
+            Log.d(LOG_TAG, "tabSelected: $position")
+        }
+
+        bottomNavigation.getTab(1)?.showBadge()?.setNumber(5)
+    }
+
+    companion object {
+        private const val LOG_TAG = "MainActivity"
     }
 }
